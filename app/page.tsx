8342d51964c6b4bdf2b1d1c3627f48abaa38335c
Revaycolizer <109684996+Revaycolizer@users.noreply.github.com>
2023-03-26 12:03:58 +0300

    Add files via upload
    First page

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,40 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import nova from '../public/nova.jpg'
+import hd from '../public/hd.png'
+import Link from 'next/link'
+
+
+export const metadata={
+  title:'nova music'
+}
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p className='text-blue-400'>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <>
+    
+    <main className={styles.fu}>
+       
+      <Image className={styles.img} src={nova} alt='nova'/>
+      <section className='lg:grid grid-cols-2 gap-10 md:flex'><span>
+      <h6 className={styles.hi}>Everyday <br/>Things are </h6>
+      {/* <h6 className={styles.hu}>Everyday <br/>Things are </h6> */}
+      <h6 className={styles.ho}>Colorful</h6>
+      <h6 className={styles.po}>Once you realize you should enjoy the small things, life changes to<br className='md:visible'></br> being boring to extremely colorful.</h6>
+      
+      <Image className={styles.im} src={hd} alt='yo'/>
+      <button className={styles.btn} >
+      <Link className={styles.btns} href='#'>Join the hub</Link>
+      </button> 
+      
+      </span>
+      <Image className='full object-cover w-96 flex justify-left items-left hidden lg:block' src={hd} alt='yo'/>
+      </section>
+     
     </main>
+    </>
   )
 }
